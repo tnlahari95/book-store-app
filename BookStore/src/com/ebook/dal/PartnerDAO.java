ad0346f9d56210869ecf6300d0304471e7d8da26
OlgaVelichko <ovelichko@luc.edu>
2021-09-26 13:50:32 -0500

    added functions to Partner facade

--- BookStore/src/com/ebook/dal/PartnerDAO.java
+++ BookStore/src/com/ebook/dal/PartnerDAO.java
@@ -1,3 +1,15 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 public class PartnerDAO{
+    public PartnerDAO(){
+
+    }
+
+    public void addPartner(PartnerInfo partner){
+
+    }
 
 }
