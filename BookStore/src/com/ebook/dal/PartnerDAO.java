0d9d983e832b9f80ea9b6f2afde366fbb88853ba
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-09-30 22:04:29 -0500

    changed JDBC to HashMap

--- BookStore/src/com/ebook/dal/PartnerDAO.java
+++ BookStore/src/com/ebook/dal/PartnerDAO.java
@@ -1,34 +1,32 @@
+package com.ebook.dal;
 
+
+import java.util.HashMap;
+
+import com.ebook.model.partner.Partner;
 
 public class PartnerDAO {
     public PartnerDAO(){}
+    
+   private static HashMap<Integer,Partner>partners = new HashMap<>();
+   
+   
+   public void addPartner(Partner partner) {
+	  int id = partner.getPartnerId();
+	  partners.put(id,partner);
+   }
+   
+   public Partner findPartnerById(int id) {
+	 return partners.get(id);
+   }
+   
+   
+   public void removePartner(Partner partner) {
+	   partners.remove(partner);
 
+   }
+    
 
-    public void addPartner(Partner partner){
-
-            Connection con = DBHelper.getConnection();
-            PreparedStatement Partner_Pst = connection.Partner_Pst("INSERT INTO partner (partnerID, name, email,password) VALUES(?, ?, ?,?)");
-            try {
-                //Insert the partner object
-                Partner_Pst.setString(1, partner.getPartnerId());
-                Partner_Pst.setString(2, partner.getPartnerName());
-                Partner_Pst.setString(3, partner.getEmail());
-                Partner_Pst.setString(4, partner.getPassword());
-                Partner_Pst.executeUpdate();
-            } catch (SQLException ex) {
-
-            } finally {
-                try {
-                    if (con != null) {
-                        con.close();
-                    }
-
-                } catch (SQLException ex) {
-                    System.err.println("PartnerDAO: Threw a SQLException saving the customer object.");
-                    System.err.println(ex.getMessage());
-                }
-            }
-        }
-    }
-
-}
+   
+   
+   
