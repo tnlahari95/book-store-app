dd1d86dd6126b3c7201b0d04ac1c72936f31b293
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-10-08 17:53:08 -0500

    add Inventory interface

---
+++ BookStore/src/com/ebook/model/partner/Inventory.java
@@ -0,0 +1,11 @@
+package com.ebook.model.partner;
+
+import com.ebook.model.item.Product;
+
+public interface Inventory {
+	public void deleteProduct(Product product);
+	public void increseQuantity(Product product, int amount);
+	public void decreaseQuantity(Product product, int amount);
+	public void reviewAllProducts();
+	public void addProduct(Product product, int quantity);
+}
