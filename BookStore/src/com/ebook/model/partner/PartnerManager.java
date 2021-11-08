967931ca27346d3b0f5e9355331acd3124633fcb
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-11-07 20:51:33 -0600

    changed to PartnerManager

--- BookStore/src/com/ebook/model/partner/PartnerManager.java
+++ BookStore/src/com/ebook/model/partner/PartnerManager.java
@@ -3,42 +3,62 @@ import java.util.Set;
 
 import com.ebook.dal.PartnerDAO;
 
-public class PartnerDAO {
-    public PartnerDAO(){}
-    
-   private static HashMap<String,Partner>partners = new HashMap<>();
-   
-   
-   public Partner addPartner(String name, String partnerInfo) {
-	   
-	  Partner partner = new Partner(name, partnerInfo);
-	  
-	  Random randomGenerator = new Random();
-	  int randomInt = randomGenerator.nextInt(10000);
-	  String id = "PI" + randomInt;
-	  
-	  partner.setpartnerId(id);
-	 
-	  partners.put(id,partner);
-	  return partner;
-   }
-   
-   public Partner findPartnerById(String id) {
-	 return partners.get(id);
-   }
-    
-   public void removePartner(String id) {
-	   partners.remove(id);
-	  
-   }
 
-   
-   public Set<Partner>getAllPartners(){
-	   Set<Partner> partners = new HashSet<>();
-	   Iterator<Partner>partner = partners.iterator();
-	   while(partner.hasNext()) {
-		   partners.add((Partner) partner);
-	   }
-	   return partners;
-   }
+
+public class PartnerManager {
+    private PartnerDAO partnerDAO = new PartnerDAO();
+
+    
+    public Partner addPartner(String name, String partnerInfo){
+        
+        Partner partner =  partnerDAO.addPartner(name,partnerInfo);
+        try {
+        	return partner;
+        }catch(Exception ex){
+            System.out.println("Partner Service: Couldn't add the partner:"+ name+" "+partnerInfo);
+
+        }
+        return null;    
+    }
+
+    public Partner findPartnerById(String partnerID){
+        try{
+            Partner partner = partnerDAO.findPartnerById(partnerID);
+            return partner;
+        }catch(Exception ex){
+            System.out.println("Partner Service: Couldn't find a partner by ID.");
+
+        }
+        return null;
+    }
+    
+    
+    public void removePartner(String id) {
+    	try {
+     		partnerDAO.removePartner(id);
+    	}catch(Exception ex) {
+    		 System.out.println("Partner Service: Couldn't delete a partner.");	
+    	}	
+    }
+    
+    
+    public Set<Partner> getAllPartners(){
+    	try {
+    		return partnerDAO.getAllPartners();	
+    	}catch(Exception ex) {
+   		 	System.out.println("Partner Service: Couldn't find the list of partners");	
+     	}
+    	return null; 	
+    }
+  
+    public Partner updatePartnerName(String partnerID, String partnerName) {
+		PartnerDAO pd = new PartnerDAO();
+		return pd.updatePartnerName(partnerID, partnerName);
+	}
+	
+	public Partner updatePartnerDetails(String partnerID, String partnerDetails) {
+		PartnerDAO pd = new PartnerDAO();
+		return pd.updatePartnerDetails(partnerID, partnerDetails);
+	}
 }
+
